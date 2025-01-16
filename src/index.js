6ec9a52a967c02fcf9a66d5f5eb7a928043eb07c
戴志文 <2649212120>
2025-01-16 21:18:38 +0800

    up:"弹出react配置文件，配置less less-loader  配置别名  配置域名以及端口号  配置跨域代理"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+// 浏览器兼容
+// import "react-app-polyfill/stable";
+// import "react-app-polyfill/ie11";
+// import "react-app-polyfill/ie9";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "@/index.less";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<div>2025React</div>);
+// fetch("/jian/subscriptions/recommended_collections")
+//   .then((res) => res.json())
+//   .then((res) => console.log(res));
+// fetch("/zhi/news/latest")
+//   .then((res) => res.json())
+//   .then((res) => console.log(res));
